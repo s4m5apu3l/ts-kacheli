3c5523f5d7afe65ea59743320f32ba8b0e3aa6ad
s4m5apu3l <asperatuz@yandex.ru>
2023-06-25 16:39:47 +0600

    feat: add news swiper adaptive

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,18 @@ let swiper = new Swiper(".js-main-banner", {
 	slidesPerView: 1,
 	loop: false,
 	init: false,
-	spaceBetween: 20,
   navigation: {
     nextEl: ".js-main-banner__next",
     prevEl: ".js-main-banner__prev",
   },
+  breakpoints: {
+    '0': {
+      spaceBetween: 8,
+    },
+    '768':{
+      spaceBetween: 20,
+    }
+  }
 	
 });
 
@@ -28,13 +35,25 @@ swiper.init();
 
 
 let swiperNewsMainPage = new Swiper(".js-swiper-news", {
-	slidesPerView: 3,
-	spaceBetween: 0,
+
   navigation: {
     nextEl: ".js-swiper-news__next",
     prevEl: ".js-swiper-news__prev ",
   },
-	
+	breakpoints: {
+    '0':{
+      slidesPerView: 1,
+      spaceBetween: 8,
+    },
+    '680':{
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    '950':{
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+  }
 });
 
 
